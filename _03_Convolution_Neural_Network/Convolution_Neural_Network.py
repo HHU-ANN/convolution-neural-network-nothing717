60143c9766d43905d7662d46d7b4722937454d17
nothing717 <2625782226@qq.com>
2023-06-28 22:30:48 +0800

    提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -19,7 +19,7 @@ vgg = [96, 96, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 
 
 class NeuralNetwork(nn.Module):
     def __init__(self, vgg):
-        super(VGG, self).__init__()
+        super(NeuralNetwork, self).__init__()
         self.features = self._make_layers(vgg)
         self.dense = nn.Sequential(
             nn.Linear(512, 4096),
