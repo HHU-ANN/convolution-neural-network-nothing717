155be5d3969ac2b5aecad786a8c8d19e096748ab
nothing717 <2625782226@qq.com>
2023-06-28 15:52:49 +0800

    提交

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -15,19 +15,72 @@ from torch.utils.data import DataLoader
     
 
 class NeuralNetwork(nn.Module):
+    def __init__(self, vgg):
+        super(VGG, self).__init__()
+        self.features = self._make_layers(vgg)
+        self.dense = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+        )
+        self.classifier = nn.Linear(4096, 10)
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.dense(out)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, vgg):
+        layers = []
+        in_channels = 3
+        for x in vgg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
     pass
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
+    transform_train = transforms.Compose(
+    [transforms.Pad(4),
+     transforms.ToTensor(),
+     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+     transforms.RandomHorizontalFlip(),
+     transforms.RandomGrayscale(),
+     transforms.RandomCrop(32, padding=4),
+    ])
+ 
+    transform_test = transforms.Compose(
+    [
+     transforms.ToTensor(),
+     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]
+    )
+
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=24, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=24, shuffle=False)
+
+    vgg = [96, 96, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
-    model = NeuralNetwork() # 若有参数则传入参数
+    model = NeuralNetwork(vgg) # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
